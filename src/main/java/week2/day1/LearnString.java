12c584505430b540c9c04271300a133877e8f614
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-06-18 10:57:45 +0530

    String code

---
+++ src/main/java/week2/day1/LearnString.java
@@ -0,0 +1,110 @@
+package week2.day1;
+
+public class LearnString {
+public static void main(String[] args) {
+	String text1 = "testleaf";
+	String text2 = new String("testleaf");
+	
+	String text = "Since 2009";
+	String replaceAll = text.replaceAll("[^0-9]", "");
+	System.out.println(replaceAll);
+	String replaceAll2 = text.replaceAll("\\D", "");
+	System.out.println(replaceAll2);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*System.out.println(text1.equals(text2));
+	String upperCase = text1.toUpperCase();
+	System.out.println(upperCase);
+	String lowerCase = upperCase.toLowerCase();
+	System.out.println(lowerCase);
+	
+	// String comparison
+	boolean equals = text1.equals(text2);
+	System.out.println(equals);
+	boolean equalsIgnoreCase = upperCase.equalsIgnoreCase(lowerCase);
+	System.out.println(equalsIgnoreCase);
+	System.out.println(upperCase.equals(lowerCase));
+	System.out.println(upperCase.toLowerCase().equals(lowerCase.toLowerCase()));
+	
+	String t1 = "Testleaf is in Chennai";
+	String t2 = "Chennai";
+	boolean contains = t1.contains(t2);
+	System.out.println(contains);
+	
+	boolean startsWith = t2.startsWith("Chenn");
+	System.out.println(startsWith);
+	
+	boolean endsWith = t2.endsWith("hennai");
+	System.out.println(endsWith);
+	
+	String concat = text1.concat(" Always ahead");
+	System.out.println(concat);
+	
+	String text = text1 + " Always Ahead";
+	System.out.println(text);
+	
+	
+	int length = text1.length();
+	
+	for (int i = 0; i < text1.length(); i++) {
+		char charAt = text1.charAt(i);
+		System.out.println(charAt);
+	}
+	
+	char[] array = text1.toCharArray();
+	for (int i = 0; i < array.length; i++) {
+		System.out.println(array[i]);
+	}
+	
+	String[] split = text1.split(" ");
+	for (int i = 0; i < split.length; i++) {
+		System.out.println(split[i]);
+	}
+	
+	String substring = text1.substring(4);
+	System.out.println(substring);
+	String substring2 = text1.substring(2, 6);
+	System.out.println(substring2);
+	
+	String replace = text1.replace("est", "GMT");
+	System.out.println(replace);
+	
+	
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
